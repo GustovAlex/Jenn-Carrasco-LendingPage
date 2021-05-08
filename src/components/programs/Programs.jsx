75231d4734a2a9754a851a0cc64b9ffff2e90d29
GustovAlex <alex.gustov.work@gmail.com>
2021-05-08 10:31:00 +0200

    small changes in styles css

--- src/components/programs/Programs.jsx
+++ src/components/programs/Programs.jsx
@@ -2,7 +2,6 @@ import './programs.css'
 import icon4 from '../../assets/icon4.png'
 import icon5 from '../../assets/icon5.png'
 import icon6 from '../../assets/icon6.png'
-import photo from '../../assets/photo.png'
 
 export default function Programs () {
 
@@ -14,20 +13,20 @@ export default function Programs () {
 						Mentorship Programs For the Elite
 					</h2>
 					<p className="programsInfoText">
-						I have three specialized programs that center around Life & Mindset, Fitness & Nutrition, <br/>
+						I have three specialized programs that center around Life & Mindset, Fitness & Nutrition,
 						and Business Mentorship. Additionally, I offer personalized one-on-one coaching. 
 					</p>
 					<div className="programsInfoItem">
 						<div className="programsInfoItemIcon">
 							<img className="programsInfoItemIconImg" src={icon4} alt="icon4"/>
 						</div>					
-						<div>
+						<div className="programsInfoItemContent">
 							<h3 className="programsInfoItemTitle">
 								Life & Mindset Coaching:
 							</h3>
 							<p className="programsInfoItemText">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br/>
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br/>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
 								ad minim veniam.
 							</p>
 						</div>
@@ -36,13 +35,13 @@ export default function Programs () {
 						<div className="programsInfoItemIcon">
 							<img className="programsInfoItemIconImg" src={icon5} alt="icon5"/>
 						</div>					
-						<div>
+						<div className="programsInfoItemContent">
 							<h3 className="programsInfoItemTitle">
 								Fitness & Nutrition:
 							</h3>
 							<p className="programsInfoItemText">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br/>
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br/>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
 								ad minim veniam.
 							</p>
 						</div>
@@ -51,21 +50,17 @@ export default function Programs () {
 						<div className="programsInfoItemIcon">
 							<img className="programsInfoItemIconImg" src={icon6} alt="icon6"/>
 						</div>					
-						<div>
+						<div className="programsInfoItemContent">
 							<h3 className="programsInfoItemTitle">
 								Business Mentorship:
 							</h3>
 							<p className="programsInfoItemText">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br/>
-								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br/>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
 								ad minim veniam.
 							</p>
 						</div>
 					</div>
-				</div>
-
-				<div>
-					<img className="programsPortret" src={photo} alt="photo"/>
 				</div>
 			</div>
 		</div>
